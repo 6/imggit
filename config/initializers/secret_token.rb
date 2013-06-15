69ffc1b4175cfda4c717c6dd15255b229886a64c
Example User <peterghm@gmail.com>
2013-06-14 22:51:45 -0700

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imggit::Application.config.secret_token = '6eb0a136c62178ef1708db0656802964300fd6b20d679045bc830460fc6bff064cb6fb1c2ee3deb20d30f52fc0d91858bdb1ca624f4bb3206bac237cc68b03c0'
