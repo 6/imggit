16fdd3f3affc67dce261277ed5b37d1108c1fe16
Example User <peterghm@gmail.com>
2013-06-14 22:52:12 -0700

    keep secret_token secret on prod

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Imggit::Application.config.secret_token = '6eb0a136c62178ef1708db0656802964300fd6b20d679045bc830460fc6bff064cb6fb1c2ee3deb20d30f52fc0d91858bdb1ca624f4bb3206bac237cc68b03c0'
+Imggit::Application.config.secret_token = ENV['SECRET_TOKEN'] || '6eb0a136c62178ef1708db0656802964300fd6b20d679045bc830460fc6bff064cb6fb1c2ee3deb20d30f52fc0d91858bdb1ca624f4bb3206bac237cc68b03c0'
