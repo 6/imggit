6357b08dc1a792be175e982d6d342253f8ae3594
Peter Graham <peterghm@gmail.com>
2013-06-16 21:44:42 -0700

    setup rspec

--- Gemfile
+++ Gemfile
@@ -6,9 +6,22 @@ gem 'jquery-rails'
 gem 'unicorn'
 gem 'newrelic_rpm'
 gem 'haml-rails'
+gem 'andand'
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
   gem 'uglifier', '>= 1.0.3'
+end
+
+group :development do
+  gem 'quiet_assets'
+  gem 'rspec-nc'
+end
+
+group :development, :test do
+  gem 'database_cleaner'
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  gem 'webmock'
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,31 @@
+require File.expand_path("../../config/environment", __FILE__)
+
+require 'rspec/rails'
+require 'rspec/autorun'
+require 'webmock/rspec'
+require 'database_cleaner'
+require 'factory_girl_rails'
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.order = "random"
+  config.use_transactional_fixtures = true
+  config.use_transactional_examples = false
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
