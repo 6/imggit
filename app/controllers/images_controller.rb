216a1ed3e7e548269bb5ded3a57e1a2a545cdcbc
Peter Graham <peterghm@gmail.com>
2013-06-25 01:24:04 -0700

    Better spacing/layout

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,7 +2,7 @@ class ImagesController < ApplicationController
   respond_to :html, :json
 
   def index
-    per_page = 10
+    per_page = 28
     offset = (params[:page].andand.to_i.andand - 1 || 0) * per_page
      respond_with(@images = ImgurImage.offset(offset).limit(per_page))
   end
