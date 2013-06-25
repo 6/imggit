adbd482cdc3f29e0b900306a9c57d52f586271db
Peter Graham <peterghm@gmail.com>
2013-06-25 01:32:17 -0700

    scrape the first 5 pages instead

--- lib/workers/reddit_listings_worker.rb
+++ lib/workers/reddit_listings_worker.rb
@@ -6,7 +6,7 @@ class RedditListingsWorker
 
     # Ensures the connection is returned back to the pool when completed
     ActiveRecord::Base.connection_pool.with_connection do
-      3.times do
+      5.times do
         scraper.listings.each do |listing|
           image = ImgurImage.find_or_initialize_by_remote_id(listing[:url].id)
           image.title = listing[:title]
