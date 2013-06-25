a378cbf3249413135711babd44f7fffccc7a7536
Peter Graham <peterghm@gmail.com>
2013-06-25 00:50:48 -0700

    whoops, fix pagination at root url

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,7 +3,7 @@ class ImagesController < ApplicationController
 
   def index
     per_page = 10
-    offset = (params[:page].andand.to_i - 1 || 0) * per_page
+    offset = (params[:page].andand.to_i.andand - 1 || 0) * per_page
      respond_with(@images = ImgurImage.offset(offset).limit(per_page))
   end
 end
