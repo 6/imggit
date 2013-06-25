4fb70e83bb3350de725281e43475d019b9a35514
Peter Graham <peterghm@gmail.com>
2013-06-25 00:10:51 -0700

    list images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,9 @@
 class ImagesController < ApplicationController
+  respond_to :html, :json
+
+  def index
+    per_page = 10
+    offset = (params[:page].andand.to_i - 1 || 0) * per_page
+     respond_with(@images = ImgurImage.offset(offset).limit(per_page))
+  end
 end
--- app/models/imgur_image.rb
+++ app/models/imgur_image.rb
@@ -1,3 +1,11 @@
 class ImgurImage < Image
   validates_length_of :remote_id, :minimum => 5
+
+  def url(size = :original)
+    ImgurUrl::Image.from_id(remote_id).url(size)
+  end
+
+  def remote_permalink
+    @remote_permalink ||= ImgurUrl::Image.from_id(remote_id).permalink_url
+  end
 end
